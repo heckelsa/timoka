1b8d1a069472d4b338a9c246089d04238a9cb3ae
Saskia Heckele <saskia.heckele@pentasys.de>
2015-10-30 15:53:47 +0100

    [Hibernate] Initialize Basic Classes

--- src/main/java/com/photo/webapp/model/Animal.java
+++ src/main/java/com/photo/webapp/model/Animal.java
@@ -1,15 +1,85 @@
 package com.photo.webapp.model;
 
-/**
- * The first sentence should be a meaningful summary of the class since it will be displayed as the class summary on the
- * Javadoc package page.
- * <p/>
- * Other notes, including guaranteed invariants, usage instructions and/or examples, reminders about desired
- * improvements, etc. should be described here.
- *
- * @author heckelsa
- * @version $Revision$ $Date$ $Author$
- */
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+@Entity
+@Table(schema = "timoka")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Access(AccessType.FIELD)
 public class Animal {
+
+    @Column(nullable = false)
+    @Id
+    @GeneratedValue
+    private int animalId;
+
+    @Column
+    private String name;
+
+    @Column
+    private int gender;
+
+    @Column
+    private String photo;
+
+    @ManyToOne
+    private Owner owner;
+
+
+    /*
+    @Column
+    private enum type {PFERD, HUND, KATZE, MAUS};
+
+    private Owner ownerId;
+    private Enum type;
+    private Enum breed;
+    private Date birthdate;
+    private int gender;
+    private Enum color;
+    */
+
+/* ===============================
+   ======  GETTER / SETTER =======
+   =============================== */
+
+    public int getAnimalId() {
+        return animalId;
+    }
+
+    public void setAnimalId(int animalId) {
+        this.animalId = animalId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
 }
--- src/main/java/com/photo/webapp/model/Owner.java
+++ src/main/java/com/photo/webapp/model/Owner.java
@@ -1,15 +1,115 @@
 package com.photo.webapp.model;
 
-/**
- * The first sentence should be a meaningful summary of the class since it will be displayed as the class summary on the
- * Javadoc package page.
- * <p/>
- * Other notes, including guaranteed invariants, usage instructions and/or examples, reminders about desired
- * improvements, etc. should be described here.
- *
- * @author heckelsa
- * @version $Revision$ $Date$ $Author$
- */
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.Collection;
 
+@Entity
+@Table(schema = "timoka")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Access(AccessType.FIELD)
 public class Owner {
+
+    @Column(nullable = false)
+    @Id
+    private int ownerId;
+
+    @Column
+    private String name;
+
+    @Column
+    private String street;
+
+    @Column
+    private String city;
+
+    @Column
+    private int postcode;
+
+    @Column
+    private String country;
+
+    @Column
+    private int phone;
+
+    @OneToMany(mappedBy = "owner")
+    private Collection<Animal> animals = new ArrayList<Animal>();
+
+
+/* ===============================
+   ======  GETTER / SETTER =======
+   =============================== */
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public Collection<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(Collection<Animal> animals) {
+        this.animals = animals;
+    }
 }
