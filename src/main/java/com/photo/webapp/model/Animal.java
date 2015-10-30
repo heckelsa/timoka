fd972e773d2ef92557239d06fd573fbe38cbc08a
Saskia Heckele <saskia.heckele@pentasys.de>
2015-10-30 14:25:13 +0100

    Initialize Empty Java Classes

---
+++ src/main/java/com/photo/webapp/model/Animal.java
@@ -0,0 +1,15 @@
+package com.photo.webapp.model;
+
+/**
+ * The first sentence should be a meaningful summary of the class since it will be displayed as the class summary on the
+ * Javadoc package page.
+ * <p/>
+ * Other notes, including guaranteed invariants, usage instructions and/or examples, reminders about desired
+ * improvements, etc. should be described here.
+ *
+ * @author heckelsa
+ * @version $Revision$ $Date$ $Author$
+ */
+
+public class Animal {
+}
