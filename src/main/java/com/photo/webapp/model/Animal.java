ee642ea3c407a7dcd77c237cd24af5a721e8c85d
Saskia Heckele <saskia.heckele@pentasys.de>
2015-11-13 14:32:49 +0100

    [Model] Refactoring Animal Model, added Equals and Hashcode

--- src/main/java/com/photo/webapp/model/Animal.java
+++ src/main/java/com/photo/webapp/model/Animal.java
@@ -137,4 +137,49 @@ public class Animal {
     public void setOwner(Owner owner) {
         this.owner = owner;
     }
+
+
+/* ===============================
+   =====  EQUALS & HASHCODE ======
+   =============================== */
+
+    /**
+     * This method needs to be checked! Probably isn't correct.
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    /**
+     * This method needs to be checked! Probably isn't correct.
+     * @param obj
+     * @return
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Animal other = (Animal) obj;
+        if (animalId >= 0) {
+            if (other.animalId >= 0)
+                return false;
+        } else if (!(animalId == other.animalId))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
 }
